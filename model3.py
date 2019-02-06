4ccee98024159a21d2b83888e11c48fab822fa56
Praneeth <pr642175@dal.ca>
2019-02-06 16:55:51 -0400

    done

---
+++ model3.py
@@ -0,0 +1,17 @@
+def individual_list():
+    f = open('running-config.cfg')
+    t1 = []
+    t2 = []
+    t3 = []
+    for line in f:
+        if 'transit_access_in' in line:
+            t1.append(line)
+        elif:
+             'fw-management_access_in' in line:
+              t2.append(line)
+        elif:
+             'global_access' in line:
+              t3.append(line)
+     print(t1)
+     print(t2)
+     print(t3)
