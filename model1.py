dc90de57cd139dbda69fe05dca598b32b47338c3
Praneeth <pr642175@dal.ca>
2019-02-06 14:08:29 -0400

    openbook task

--- model1.py
+++ model1.py
@@ -1,12 +1,41 @@
-def list_ipname_ip(interface,name):
-	d={}
-	ipaddress,netmask=interface.split(" ")
-	d[name]=(ipaddress,netmask)
-	print(d)
+l=[]
+with open("running-config.cfg","rt") as in_file:
+	for interface in in_file:
+		l.append(interface)
 
-fout=open("running-config.cfg")
-for interface in fout:
-	if interface!= "!":
 
-	else:
-           break
+interface=[]
+vlan=[]
+name=[]
+ip_address=[]
+mac_address=[]
+for i in l:
+	a=""
+	b=""
+	t=i.split()
+	
+	if(t[0]=="interface"):
+		interface.append(t[1])
+	if(t[0]=="vlan"):
+		vlan.append(t[1])
+	if(t[0]=="nameif"):
+		name.append(t[1])
+	if(t[0]=="ip"):
+		ip_address.append(t[2])
+		mac_address.append(t[3])
+	
+
+print("Interface")
+print(interface)
+
+print("Vlan")
+print(vlan)
+
+print("Name")
+print(name)
+
+print("Ip address")
+print(ip_address)
+
+print("Mac addres")
+print(mac_address)
