1a9fa01b9fbfcaffd56630575db685e10cae32c9
Praneeth <pr642175@dal.ca>
2019-02-06 14:19:47 -0400

    openbook task

---
+++ model2.py
@@ -0,0 +1,7 @@
+
+with open('running-config.cfg') as f:
+ newText=f.read().replace('192', '10')
+ newText=f.read().replace('172', '10')
+with open('running-config.cfg', "w") as f:
+ f.write(newText)
+print(f)
